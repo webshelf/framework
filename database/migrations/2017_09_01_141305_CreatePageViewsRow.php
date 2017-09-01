9f95f88e6dfdbd1a1ecc8d507c16c58208963456
Mark Hester <marky360@live.ie>
2017-09-01 16:30:20 +0100

    plugin page view hits row.

---
+++ database/migrations/2017_09_01_141305_CreatePageViewsRow.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePageViewsRow extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function(Blueprint $table){
+           $table->integer('views')->after('seo_keywords')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
