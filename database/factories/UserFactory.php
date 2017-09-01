8b7535c596c6c7553745f2698f62d1bcc251300e
Mark Hester <marky360@live.ie>
2017-09-01 16:31:13 +0100

    Page::class factory seeder.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,7 +13,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(\App\Model\Account::class, function (Faker $faker) {
     static $password;
 
     return [
@@ -23,3 +23,23 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(\App\Model\Page::class, function (Faker $faker) {
+
+    $title = $faker->sentence(5);
+
+    return [
+        'slug' => str_slug($title),
+        'content' => $faker->sentence(300),
+        'seo_title' => $title,
+        'seo_description' => $faker->sentence(),
+        'seo_keywords' => $faker->sentence(5),
+        'views' => $faker->numberBetween(0, 999),
+        'sitemap' => $faker->boolean(50),
+        'enabled' => $faker->boolean(100),
+        'created_at' => $faker->dateTimeBetween('-2 years', '-11 months'),
+        'updated_at' => $faker->dateTimeBetween('-10 months'),
+        'creator_id' => 1,
+    ];
+});
+
