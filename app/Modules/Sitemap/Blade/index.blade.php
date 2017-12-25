7964dcbbc9de12c779cb8b04ce7a7c309bcd5b6e
Mark Hester <marky360@live.ie>
2017-12-25 00:46:34 +0000

    sitemap better display.

---
+++ app/Modules/Sitemap/Blade/index.blade.php
@@ -0,0 +1,34 @@
+@extends('dashboard::frame')
+
+@section('title')
+    Sitemap
+@endsection
+
+@section('information')
+    What search engines see when they visit your site.
+@endsection
+
+@section('content')
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">Location</th>
+            <th scope="col">Modified</th>
+            <th scope="col">Checkup</th>
+            <th scope="col">Priority</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($sitemaps as $sitemap)
+            <tr>
+                <td>/{{ $sitemap['url_location'] }}</td>
+                <td>{{ $sitemap['last_modified'] }}</td>
+                <td>{{ $sitemap['change_frequency'] }}</td>
+                <td>{{ $sitemap['priority'] }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+@endsection
--- app/Modules/Sitemap/Controller.php
+++ app/Modules/Sitemap/Controller.php
@@ -61,7 +61,7 @@ class Controller extends ModuleEngine
         // get and create an array of all the sitemaps to be loaded
         // send to the view for display.
 
-        return $this->make('iframe')->with('sitemaps', $this->sitemap->generateArray());
+        return $this->make('index')->with('sitemaps', $this->sitemap->generateArray());
     }
 
     /**
