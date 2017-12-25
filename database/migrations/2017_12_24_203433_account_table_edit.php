19379da4afc2985160ca669bb73d803d8f2ca294
Mark Hester <marky360@live.ie>
2017-12-25 00:37:34 +0000

    allow database seeding for better testing.

---
+++ database/migrations/2017_12_24_203433_account_table_edit.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AccountTableEdit extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->string('number')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Model\Page;
+use App\Model\Menu;
+use App\Model\Account;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(FrameworkTableSeeder::class);
+        factory(Account::class, 35)->create();
+
+        factory(Page::class, 75)->create();
+
+        factory(Menu::class, rand(4,7))->states('parent')->create();
+
+        factory(Menu::class, 32)->create();
+
+        $account = new Account;
+        $account->forename = 'Super';
+        $account->surname = 'User';
+        $account->verified = true;
+        $account->role_id = 1;
+        $account->password = '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm'; //secret
+        $account->email = 'webshelf@test.net';
+        $account->save();
     }
 }
