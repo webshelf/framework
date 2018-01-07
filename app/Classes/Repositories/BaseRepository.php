bad6bb23f7c027269f02dada489d5420241e1170
marky291 <Marky360@live.ie>
2018-01-07 23:05:18 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Classes/Repositories/BaseRepository.php
+++ app/Classes/Repositories/BaseRepository.php
@@ -3,22 +3,19 @@
  * Created by PhpStorm.
  * User: Marky
  * Date: 04/01/2018
- * Time: 00:13
+ * Time: 00:13.
  */
 
 namespace App\Classes\Repositories;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
- * Class BaseRepository
- *
- * @package App\Classes\Repositories
+ * Class BaseRepository.
  */
 class BaseRepository
 {
-
     /**
      * @var Builder|Collection
      */
@@ -40,5 +37,4 @@ class BaseRepository
     {
         return $this->model->where('id', $integer)->first();
     }
-
 }
