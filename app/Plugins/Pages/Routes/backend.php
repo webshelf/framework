d6a3be7f9ef2ebc9fea7bc11b06110275d42125b
marky291 <Marky360@live.ie>
2018-03-24 16:13:00 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Plugins/Pages/Routes/backend.php
+++ app/Plugins/Pages/Routes/backend.php
@@ -16,7 +16,6 @@
     Route::get('/admin/pages/special')->uses('BackendController@indexSpecial')->name('admin.pages.special');
     Route::get('/admin/pages/normal')->uses('BackendController@index')->name('admin.pages.index');
 
-
     Route::resource('admin/pages', 'BackendController', ['as' => 'admin']);
     // Allow destruction without using forms.
     Route::get('/admin/pages/destroy/{slug}')->uses('BackendController@destroy')->name('admin.pages.destroy');
