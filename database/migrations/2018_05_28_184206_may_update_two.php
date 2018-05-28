8a530cd3bbd92728c4c714f3fdf9bfbd5a8ddf64
Mark Hester <marky360@live.ie>
2018-05-28 20:24:19 +0100

    Default user account on new migration setup.

--- database/migrations/2018_05_28_184206_may_update_two.php
+++ database/migrations/2018_05_28_184206_may_update_two.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use App\Model\Account;
 
 class MayUpdateTwo extends Migration
 {
@@ -13,6 +15,22 @@ class MayUpdateTwo extends Migration
     public function up()
     {
         Schema::dropIfExists('audits');
+
+        
+        // Create a new standard account for new tables.
+        $account = Account::find(1);
+
+        if (!$account) {
+            $account = new Account;
+            $account->setAttribute('forename', 'Root');
+            $account->setAttribute('surname', 'Account');
+            $account->setAttribute('email', 'webshelf@live.ie');
+            $account->setAttribute('verified', true);
+            $account->setAttribute('status', true);
+            $account->setAttribute('role_id', true);
+            $account->setAttribute('password', bcrypt('password'));
+            $account->save();
+        }
     }
 
     /**
